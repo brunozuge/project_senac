6873f85b0b6763a399c259b9c5ff02593e590684
ZZZ <brunorzuge@gmail.com>
2025-04-14 20:11:37 -0300

    Delete controle directory

--- controle/controle_session.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['controle_login']) || $_SESSION['controle_login'] !== true) {
-    header('Location: ../visao/login.php?erro=sem_acesso');
-    exit();
-}
-?>
